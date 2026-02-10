3e7efae1f501513c46ceea1f5bf9f35152954e41
biswasuser <lohanibiswas1@gmail.com>
2026-02-10 13:28:23 +0545

    completed hero section

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -20,6 +20,20 @@ const Hero = () => {
             <button className='bg-blue-600 px-6 py-2 text-white rounded m-1 hover:bg-blue-500 cursor-pointer'>Search</button>
         </div>
       </div>
+
+      {/* Company logo section */}
+      <div  className='border border-gray-300 shadow-md mx-2 mt-5 p-6 rounded-md flex'>
+        <div  className='flex items-center justify-center gap-10 lg:gap-16 flex-wrap'>
+            <p className='font-medium'>Trusted by</p>
+            <img className='h-6' src={assets.microsoft_logo} alt="" />
+            <img className='h-6' src={assets.walmart_logo} alt="" />
+            <img className='h-6' src={assets.accenture_logo} alt="" />
+            <img className='h-6' src={assets.samsung_logo} alt="" />
+            <img className='h-6' src={assets.amazon_logo} alt="" />
+            <img className='h-6' src={assets.adobe_logo} alt="" />
+        </div>
+      </div>
+
     </div>
   )
 }
