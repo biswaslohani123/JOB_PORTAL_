75009723a3002973e14d07e70ed3fa3752c8b987
biswasuser <lohanibiswas1@gmail.com>
2026-02-12 21:11:38 +0545

    completed footer

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import NavBar from '../components/NavBar'
 import Hero from '../components/Hero'
 import JobListing from '../components/JobListing'
 import Banner from '../components/Banner'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Hero/>
       <JobListing/>
       <Banner/>
+      <Footer/>
     </div>
   )
 }
