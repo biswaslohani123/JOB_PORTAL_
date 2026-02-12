ccca5298dcc0221994d7065f457dc65c8a924891
biswasuser <lohanibiswas1@gmail.com>
2026-02-12 20:35:50 +0545

    completed filter and job section

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useRef } from 'react'
 import { assets } from '../assets/assets'
 import { AppContext } from '../context/AppContext'
+import { motion } from "framer-motion";
 
 const Hero = () => {
 
@@ -45,17 +46,33 @@ const Hero = () => {
       </div>
 
       {/* Company logo section */}
-      <div  className='border border-gray-300 shadow-md mx-2 mt-5 p-6 rounded-md flex'>
-        <div  className='flex items-center justify-center gap-10 lg:gap-16 flex-wrap'>
-            <p className='font-medium'>Trusted by</p>
-            <img className='h-6' src={assets.microsoft_logo} alt="" />
-            <img className='h-6' src={assets.walmart_logo} alt="" />
-            <img className='h-6' src={assets.accenture_logo} alt="" />
-            <img className='h-6' src={assets.samsung_logo} alt="" />
-            <img className='h-6' src={assets.amazon_logo} alt="" />
-            <img className='h-6' src={assets.adobe_logo} alt="" />
-        </div>
-      </div>
+       <div className='border border-gray-300 shadow-md mx-2 mt-5 p-6 rounded-md overflow-hidden'>
+      <motion.div
+        className='flex items-center gap-10 lg:gap-16'
+        animate={{ x: ["0%", "-100%"] }}
+        transition={{
+          duration: 12,
+          repeat: Infinity,
+          ease: "linear"
+        }}
+      >
+        <p className='font-medium'>Trusted by</p>
+        <img className='h-6' src={assets.microsoft_logo} alt="" />
+        <img className='h-6' src={assets.walmart_logo} alt="" />
+        <img className='h-6' src={assets.accenture_logo} alt="" />
+        <img className='h-6' src={assets.samsung_logo} alt="" />
+        <img className='h-6' src={assets.amazon_logo} alt="" />
+        <img className='h-6' src={assets.adobe_logo} alt="" />
+
+        {/* Duplicate logos for seamless loop */}
+        <img className='h-6' src={assets.microsoft_logo} alt="" />
+        <img className='h-6' src={assets.walmart_logo} alt="" />
+        <img className='h-6' src={assets.accenture_logo} alt="" />
+        <img className='h-6' src={assets.samsung_logo} alt="" />
+        <img className='h-6' src={assets.amazon_logo} alt="" />
+        <img className='h-6' src={assets.adobe_logo} alt="" />
+      </motion.div>
+    </div>
 
     </div>
   )
---
+++ frontend/src/components/JobListing.jsx
@@ -0,0 +1,102 @@
+import React, { useContext, useState } from 'react'
+import { AppContext } from '../context/AppContext'
+import { assets, JobCategories, JobLocations} from '../assets/assets'
+import JobCard from './JobCard'
+
+const JobListing = () => {
+
+
+    const {isSearched, searchFilter, setSearchFilter, jobs} = useContext(AppContext)
+
+    const [showFilter, setShowFilter] = useState(false)
+
+    
+
+  return (
+    <div className='container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8'>
+
+      {/* SideBar */}
+      <div className='w-full lg:w-1/4 bg-white px-4'>
+
+        {/* Search Filter from Hero component */}
+        {
+            isSearched && (searchFilter.title !== "" || searchFilter.location !== "") &&
+            (
+                <>
+                    <h3 className='font-medium text-lg mb-4'>Current Search</h3>
+                    <div className='mb-4 text-gray-600'>
+                        {searchFilter.title && (
+                            <span className='inline-flex items-center gap-2.5 bg-blue-50 border border-blue-100 px-4 py-1.5 rounded'>
+                                {searchFilter.title}
+                                <img onClick={(e) => setSearchFilter(prev => ({...prev, title: ''})) } className='cursor-pointer' src={assets.cross_icon} alt="" />
+                            </span>
+                        )}
+                        {searchFilter.title && (
+                            <span className='inline-flex items-center gap-2.5 bg-red-50 border ml-2 border-red-300 px-4 py-1.5 rounded'>
+                                {searchFilter.location}
+                                <img onClick={(e) => setSearchFilter(prev => ({...prev, location: ''})) }  className='cursor-pointer' src={assets.cross_icon} alt="" />
+                            </span>
+                        )}
+                    </div>
+                </>
+            )
+        }
+
+        <button onClick={(e) => setShowFilter(prev => !prev)} className='px-6 py-1.5 rounded border border-gray-400 lg:hidden'>
+            {showFilter ? 'Close' : "Filters"}
+        </button>
+
+        {/* Category filter */}
+        <div className={showFilter ? "": "max-lg:hidden"}>
+            <h4 className='font-medium text-lg py-4'>Search by Categories</h4>
+            <ul className='space-y-4 text-gray-600'>
+                {
+                    JobCategories.map((category, index) => (
+                        <li className='flex gap-3 items-center' key={index}>
+
+                            <input className='scale-124' type="checkbox" />
+                            {category}
+
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+
+
+        {/* Search By Location filter */}
+        <div className={showFilter ? "": "max-lg:hidden"}>
+            <h4 className='font-medium text-lg py-4 pt-14'>Search by Job Locations</h4>
+            <ul className='space-y-4 text-gray-600'>
+                {
+                    JobLocations.map((location, index) => (
+                        <li className='flex gap-3 items-center' key={index}>
+
+                            <input className='scale-124' type="checkbox" />
+                            {location}
+
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+      </div>
+
+     {/* Job Listings */}
+     <section className='w-full lg:w-3/4 text-gray-800 max-lg:px-4'>
+        <h3 className='font-medium text-3xl py-2 ' id='job-list'>Latest Jobs</h3>
+        <p className='mb-8'>Get your desired job from the top companies.</p>
+        <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4'>
+           {
+            jobs.map((job, index) => (
+                <JobCard key={index} job={job}/>
+            ))
+           }
+        </div>
+     </section>
+
+    </div>
+  )
+}
+
+export default JobListing
